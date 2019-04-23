b2f9070742ed0095c4dcc98c22193afcd55930ea
Gavin Stark <gavin@gstark.com>
2019-04-23 11:28:30 -0400

    Uses component for location

--- .components/ReadingNav.js
+++ .components/ReadingNav.js
@@ -1,32 +1,59 @@
 import React from 'react'
+import { Location } from '@reach/router'
 
 const ReadingNav = props => {
   const { pages } = props
 
-  const path = window.location.pathname.split('/').pop()
-  const index = pages.findIndex(page => page.path === path)
+  const findIndex = location => {
+    console.log(location)
 
-  const nextPath = pages[index + 1] && `./${pages[index + 1].path}`
-  const nextText = pages[index + 1] && pages[index + 1].text
+    const path = location.pathname.split('/').pop()
+    return pages.findIndex(page => page.path === path)
+  }
 
-  const prevPath = pages[index - 1] && `./${pages[index - 1].path}`
-  const prevText = pages[index - 1] && pages[index - 1].text
+  const nextPath = location => {
+    const index = findIndex(location)
+
+    return pages[index + 1] && `./${pages[index + 1].path}`
+  }
+
+  const nextText = location => {
+    const index = findIndex(location)
+
+    return pages[index + 1] && pages[index + 1].text
+  }
+
+  const prevPath = location => {
+    const index = findIndex(location)
+
+    return pages[index - 1] && `./${pages[index - 1].path}`
+  }
+
+  const prevText = location => {
+    const index = findIndex(location)
+
+    return pages[index - 1] && pages[index - 1].text
+  }
 
   return (
-    <ul className="handbook-nav-links">
-      {nextPath && (
-        <li>
-          <i className="fa fa-angle-right" />
-          <a href={nextPath}>{nextText}</a>
-        </li>
+    <Location>
+      {({ location }) => (
+        <ul className="handbook-nav-links">
+          {nextPath(location) && (
+            <li>
+              <i className="fa fa-angle-right" />
+              <a href={nextPath(location)}>{nextText(location)}</a>
+            </li>
+          )}
+          {prevPath(location) && (
+            <li>
+              <i className="fa fa-angle-left" />
+              <a href={prevPath(location)}>{prevText(location)}</a>
+            </li>
+          )}
+        </ul>
       )}
-      {prevPath && (
-        <li>
-          <i className="fa fa-angle-left" />
-          <a href={prevPath}>{prevText}</a>
-        </li>
-      )}
-    </ul>
+    </Location>
   )
 }
 
