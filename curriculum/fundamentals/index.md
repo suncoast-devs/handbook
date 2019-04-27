a8bde317e031776d228057ca39b00654fdf654b4
Gavin Stark <gavin@gstark.com>
2019-04-27 10:52:24 -0400

    Refactors into unit/module/lesson

--- curriculum/fundamentals/index.md
+++ curriculum/fundamentals/index.md
@@ -1,7 +1,14 @@
 # Web Development Fundamentals
 
-## Lessons
+## HTML & CSS
 
-- [Intro To HTML](/handbook/curriculum/fundamentals/lessons/intro-to-html)
-- [Intro to CSS](/handbook/curriculum/fundamentals/lessons/intro-to-css)
-- [CSS Layout](/handbook/curriculum/fundamentals/lessons/css-layout)
+- [Intro To HTML](/handbook/curriculum/fundamentals/modules/html-css/lessons/intro-to-html)
+- [Intro to CSS](/handbook/curriculum/fundamentals/modules/html-css/lessons/intro-to-css)
+- [CSS Layout](/handbook/curriculum/fundamentals/modules/html-css/lessons/css-layout)
+- [Intro to Responsive Web](/handbook/curriculum/fundamentals/modules/html-css/lessons/intro-to-responsive)
+
+**End Of Module Project**: [The Final Countdown](/handbook/curriculum/fundamentals/projects/the-final-countdown)
+
+## JavaScript
+
+- [Blah]()
--- make-lesson.sh
+++ make-lesson.sh
@@ -4,7 +4,8 @@
 set -e
 
 UNIT="$1"
-LESSON_NAME="$2"
+MODULE="$2"
+LESSON_NAME="$3"
 
 # Prompt if missing
 if [ -z "${UNIT}" ]; then
@@ -18,13 +19,20 @@ if [ ! -d ./curriculum/${UNIT} ]; then
 fi
 
 # Prompt if missing
+if [ -z "${MODULE}" ]; then
+  /bin/echo -n "Enter the name of the module: "
+  read MODULE_NAME
+fi
+
+# Prompt if missing
 if [ -z "${LESSON_NAME}" ]; then
   /bin/echo -n "Enter the name of the lesson: "
   read LESSON_NAME
 fi
 
 # Slugify
+MODULE_NAME=$(echo $MODULE_NAME | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z)
 LESSON_NAME=$(echo $LESSON_NAME | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z)
 
-mkdir -p curriculum/${UNIT}/lessons/${LESSON_NAME}
-cp -n -a .lesson-template/* curriculum/${UNIT}/lessons/${LESSON_NAME}
+mkdir -p curriculum/${UNIT}/modules/${MODULE_NAME}/lessons/${LESSON_NAME}
+cp -n -a .lesson-template/* curriculum/${UNIT}/modules/${MODULE_NAME}/lessons/${LESSON_NAME}
