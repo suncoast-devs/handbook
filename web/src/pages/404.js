46073f68e06432f634dfe11b06644917a9541b33
Jason L Perry <jason@suncoast.io>
2020-03-26 22:51:33 -0400

    Add gatsby

---
+++ web/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const NotFoundPage = () => (
+  <Layout>
+    <h1>NOT FOUND</h1>
+  </Layout>
+)
+
+export default NotFoundPage
