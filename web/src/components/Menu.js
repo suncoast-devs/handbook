388ced64863829f8bf71d1c5865e90d5b4022e7f
Jason L Perry <jason@suncoast.io>
2020-05-01 01:15:04 -0400

    Mock up navigation menu

---
+++ web/src/components/Menu.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react'
+import cx from 'classnames'
+import { Transition } from './Transition'
+
+function SectionHeading({ children }) {
+  return <h4 className="mx-6 mt-3 font-semibold">{children}</h4>
+}
+
+export function Menu() {
+  const [module, setModule] = useState(0)
+
+  function Heading({ children, onClick, icon }) {
+    return (
+      <h3 className="pl-6 pr-3 flex items-center text-xs leading-4 font-semibold text-gray-500 uppercase tracking-wider">
+        {icon && (
+          <span className="mr-2">
+            <i className={icon}></i>
+          </span>
+        )}
+        <a
+          href={onClick && '#'}
+          onClick={onClick}
+          className={cx({ 'hover:text-gray-400': onClick })}
+        >
+          {children}
+        </a>
+      </h3>
+    )
+  }
+
+  function Item({ children }) {
+    return (
+      <li
+        className="mt-1 group flex items-center justify-between px-3 py-2 text-sm leading-5 font-medium text-gray-400 hover:text-gray-200 rounded-l-md hover:bg-gray-600 focus:outline-none focus:bg-gray-100 transition ease-in-out duration-150"
+        onClick={() => setModule(1)}
+      >
+        {children}
+        <i className="far fa-angle-right"></i>
+      </li>
+    )
+  }
+
+  function ProgramMenu() {
+    return (
+      <>
+        <Heading icon="far fa-house">All Programs</Heading>
+        <nav className="mb-6">
+          <SectionHeading>Web Development Program</SectionHeading>
+          <ul className="pl-3">
+            <Item>Introduction to Programming with C#</Item>
+            <Item>Fundamentals of Front-end Web Development</Item>
+          </ul>
+        </nav>
+        <nav>
+          <SectionHeading>Web Development Test Drive</SectionHeading>
+          <ul className="pl-3">
+            <Item>HTML: Structure & Semantics</Item>
+            <Item>CSS: Style & Presentation</Item>
+            <Item>JavaScript: Behavior & Interaction</Item>
+          </ul>
+        </nav>
+      </>
+    )
+  }
+
+  function ModuleMenu() {
+    return (
+      <>
+        <Heading onClick={() => setModule(0)} icon="fas fa-caret-left">
+          Web Development Test Drive
+        </Heading>
+        <SectionHeading>HTML: Structure & Semantics</SectionHeading>
+        <div className="m-3 p-3 text-sm leading-tight text-gray-300 bg-gray-700 rounded">
+          <p>
+            HTML (HyperText Markup Language) is the most basic building block of
+            the Web. It defines the meaning and structure of web content.
+          </p>
+        </div>
+        <Heading>Lessons</Heading>
+        <ul className="pl-3">
+          <Item>Introduction to HTML</Item>
+        </ul>
+      </>
+    )
+  }
+
+  return (
+    <div className="pt-6 flex-1 h-0 overflow-y-auto bg-gray-800 text-white relative">
+      <Transition
+        enter="transition ease-in-out duration-300 transform"
+        enterFrom="-translate-x-full"
+        enterTo="translate-x-0"
+        leave="transition ease-in-out duration-300 transform"
+        leaveFrom="translate-x-0"
+        leaveTo="-translate-x-full"
+        show={module === 0}
+      >
+        <div className="absolute left-0 right-0">
+          <ProgramMenu />
+        </div>
+      </Transition>
+      <Transition
+        enter="transition ease-in-out duration-300 transform"
+        enterFrom="translate-x-full"
+        enterTo="translate-x-0"
+        leave="transition ease-in-out duration-300 transform"
+        leaveFrom="translate-x-0"
+        leaveTo="translate-x-full"
+        show={module === 1}
+      >
+        <div className="absolute left-0 right-0">
+          <ModuleMenu />
+        </div>
+      </Transition>
+    </div>
+  )
+}
--- web/src/components/Navigation.js
+++ web/src/components/Navigation.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import cx from 'classnames'
+import { Menu } from './Menu'
 import SDGIcon from '../images/button.svg'
-import { Transition } from './Transition.js'
+import { Transition } from './Transition'
 import { useUIContext } from './UIContext'
 
 function Header() {
@@ -25,8 +27,7 @@ export function Navigation() {
 
   return (
     <>
-      {/* Off-canvas menu for mobile */}
-      <div className={`${isSidebarHidden ? 'hidden ' : ''}md:hidden`}>
+      <div className={cx({ hidden: isSidebarHidden }, 'md:hidden')}>
         <div className="fixed inset-0 flex z-40">
           <Transition
             enter="transition-opacity ease-linear duration-300"
@@ -51,7 +52,7 @@ export function Navigation() {
             leaveTo="-translate-x-full"
             show={isSidebarOpen}
           >
-            <div className="relative flex-1 flex flex-col max-w-xs w-full pb-4 bg-gray-800">
+            <div className="relative flex-1 flex flex-col max-w-xs w-full">
               <div className="absolute top-0 right-0 -mr-14 p-1">
                 <button
                   className="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
@@ -62,54 +63,16 @@ export function Navigation() {
                 </button>
               </div>
               <Header />
-              <div className="mt-5 flex-1 h-0 overflow-y-auto">
-                <nav className="px-2">
-                  <a
-                    href="#"
-                    className="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-white bg-gray-900 focus:outline-none focus:bg-gray-700 transition ease-in-out duration-150"
-                  >
-                    <i className="mr-4 text-gray-300 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150 far fa-house"></i>
-                    Dashboard
-                  </a>
-                  <a
-                    href="#"
-                    className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-                  >
-                    <i className="mr-4 text-gray-300 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150 far fa-users"></i>
-                    Team
-                  </a>
-                </nav>
-              </div>
+              <Menu />
             </div>
           </Transition>
-          <div className="flex-shrink-0 w-14">
-            {/* Dummy element to force sidebar to shrink to fit close icon */}
-          </div>
+          <div className="flex-shrink-0 w-14"></div>
         </div>
       </div>
-      {/* Static sidebar for desktop */}
       <div className="hidden md:flex md:flex-shrink-0">
         <div className="flex flex-col w-64">
           <Header />
-          <div className="h-0 flex-1 flex flex-col overflow-y-auto">
-            {/* Sidebar component, swap this element with another sidebar if you like */}
-            <nav className="flex-1 px-2 py-4 bg-gray-800">
-              <a
-                href="#"
-                className="group flex items-center px-2 py-2 text-sm leading-5 font-medium text-white rounded-md bg-gray-900 focus:outline-none focus:bg-gray-700 transition ease-in-out duration-150"
-              >
-                <i className="mr-3 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150 far fa-house"></i>
-                Dashboard
-              </a>
-              <a
-                href="#"
-                className="mt-1 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-gray-300 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition ease-in-out duration-150"
-              >
-                <i className="mr-3 text-gray-400 group-hover:text-gray-300 group-focus:text-gray-300 transition ease-in-out duration-150 far fa-users"></i>
-                Team
-              </a>
-            </nav>
-          </div>
+          <Menu />
         </div>
       </div>
     </>
