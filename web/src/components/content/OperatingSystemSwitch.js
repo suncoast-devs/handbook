25c3adbe1c03d64789edc63b087d8e2d622536a3
Gavin Stark <gavin@gstark.com>
2021-03-21 18:02:00 -0400

    Show which OS is highlighted

--- web/src/components/content/OperatingSystemSwitch.js
+++ web/src/components/content/OperatingSystemSwitch.js
@@ -6,15 +6,23 @@ export const OperatingSystemContext = React.createContext({
 })
 
 export function OperatingSystemSelector() {
-  const { setOS } = useContext(OperatingSystemContext)
+  const { os, setOS } = useContext(OperatingSystemContext)
 
   return (
     <div className="flex justify-end my-1">
       <span onClick={() => setOS('Mac')}>
-        <i className="fal fa-fw fa-2x fa-apple-alt cursor-pointer" />
+        <i
+          className={`pb-2 border-b-4 border-${
+            os === 'Mac' ? 'indigo-600' : 'transparent'
+          } fal fa-fw fa-2x fa-apple-alt cursor-pointer`}
+        />
       </span>
       <span onClick={() => setOS('Windows')}>
-        <i className="fab fa-fw  fa-2x fa-windows cursor-pointer" />
+        <i
+          className={`pb-2 border-b-4 border-${
+            os === 'Windows' ? 'indigo-600' : 'transparent'
+          } fab fa-fw  fa-2x fa-windows cursor-pointer`}
+        />
       </span>
     </div>
   )
