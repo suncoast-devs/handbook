8a7db93a019d2faae6a5275c235413d66b20cbff
Gavin Stark <gavin@gstark.com>
2021-04-05 11:42:37 -0400

    Using bg-purple-800, same as header

--- web/src/components/content/SectionSeparator.js
+++ web/src/components/content/SectionSeparator.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export function SectionSeparator() {
-  return <div className="mt-48 border-t-8 border-indigo-600" />
+  return <div className="mt-48 border-t-8 bg-purple-800" />
 }
